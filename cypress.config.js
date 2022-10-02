200dbfd1e5b7c53efb2085590ab0d5b6bd104de5
KlimovAndrii <ag.klimov@gmail.com>
2022-10-02 19:51:37 +0300

    modified project

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      on('file:preprocessor', webpackPreprocessor());
     },
     baseUrl: "https://telnyx.com/",
   },
